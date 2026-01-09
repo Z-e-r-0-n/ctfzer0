219d80464fd8d376c80e9fb32b62268950cdb56d
Z-e-r-0-n <kazuto.iii.kirigaya@gmail.com>
2026-01-09 13:29:55 +0530

    shell config and directory for library and meta data

---
+++ replica/etc/profile.d/shell-config.sh
@@ -0,0 +1,9 @@
+#global shell config
+alias ls='ls --color=auto'
+alias ll='ls -lah --color=auto'
+alias grep='grep --color=auto'
+alias cls='clear'
+alias poweroff='shutdown now'
+alias reboot='reboot'
+export YOUROS=1
+
