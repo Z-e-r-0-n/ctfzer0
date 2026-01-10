d4b7701274cf1c68bfd28a36989cc01614a48b0d
Z-e-r-0-n <kazuto.iii.kirigaya@gmail.com>
2026-01-10 12:13:48 +0530

    hhmmmmmm

--- replica/etc/profile.d/shell-config.sh
+++ replica/etc/profile.d/shell-config.sh
@@ -5,5 +5,11 @@ alias grep='grep --color=auto'
 alias cls='clear'
 alias poweroff='shutdown now'
 alias reboot='reboot'
+# just colour diff between root and non root
+if [ "$(id -u)" -eq 0 ]; then
+    PS1='\[\e[31m\][ROOT@\h \W]#\[\e[0m\] '
+else
+    PS1='\[\e[32m\][\u@\h \W]$\[\e[0m\] '
+fi
 export YOUROS=1
 
